0590aa55f13ed27b565c953660d92cbee6d0be5e
Your Name <nguyenlamvien0110@gmail.com>
2020-11-18 16:16:49 +0700

    fix segmentation fault

--- OLP_HCMUT/Bai4/Bai4.c
+++ OLP_HCMUT/Bai4/Bai4.c
@@ -29,45 +29,57 @@ int main()
     }
     fscanf(fptr_in, "%u", &n);
     unsigned int **graph;
-    graph=(unsigned int **)malloc(n*sizeof(unsigned int));
-    for (int i=0; i<n;i++)
+    graph = (unsigned int **)malloc(n * sizeof(unsigned int *));
+    for (int i = 0; i < n; i++)
     {
-        *(graph+i)=(int *)malloc(n*sizeof(unsigned int));
+        *(graph + i) = (unsigned int *)malloc(n * sizeof(unsigned int));
     }
     unsigned int row = 0, column = 0;
 
     // All element of undirected graph in City.inp file
-    for (int i = 0; i < Factorial(n)/(2*Factorial(n-2)); i++)
+    for (int i = 0; i < Factorial(n) / (2 * Factorial(n - 2)); i++)
     {
         fscanf(fptr_in, "%u", &row);
         fscanf(fptr_in, "%u", &column);
-        fscanf(fptr_in, "%u", (*(graph + row) + column));
-        *(*(graph+column)+row)=*(*(graph+row)+column);
-        //DBG("%u\t%u\n", *(*(graph + row) + column),*(*(graph+column)+row));
+        fscanf(fptr_in, "%u", (*(graph + row - 1) + column - 1));
+        *(*(graph + column - 1) + row - 1) = *(*(graph + row - 1) + column - 1);
+        //DBG("%u\t%u\n", *(*(graph + row - 1) + column - 1), *(*(graph + column - 1) + row - 1));
     }
+    //print_array(graph, n);
+    for (int i = 0; i < n; i++)
+    {
+        *(*(graph + i) + i) = 0;
+    }
+    /*     for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%u\t", *(*(graph + i) + j));
+        }
+        printf("\n");
+    } */
+    DBG("Undirected Graph:\n");
     print_array(graph, n);
-    
 
     return 0;
 }
 
 unsigned int Factorial(unsigned int n)
 {
-    if (n==1)
+    if (n == 1)
     {
         return 1;
     }
-    return n*Factorial(n-1);
+    return n * Factorial(n - 1);
 }
 
 void print_array(unsigned int **arr, unsigned int n)
 {
-
-    for (int i=0; i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            printf("%u\t", *(*(arr+i)+j));
+            printf("%u\t", *(*(arr + i) + j));
         }
         printf("\n");
     }
